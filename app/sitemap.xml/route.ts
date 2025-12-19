cc7d1fe8eb0c4017f581267ba1f40194b4b3c245
Error404-NNF <silumamuziwakhe23@gmail.com>
2025-12-19 22:54:25 +0200

    Update route.ts

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -14,9 +14,7 @@ export async function GET() {
   }).join('');
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls}
-</urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`;
 
   return new Response(sitemap, {
     headers: {
